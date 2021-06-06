63d693107bc680dbaf117fd0ddb54faf91f9ba5d
alanNunes <alan.fsa.ans@gmail.com>
2021-06-06 18:32:03 -0300

    rota hellor word

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
   res.send("Hello na aws");
 })
 
+app.get("/teste", function(req, res){
+  res.send("Hello word");
+})
+
 app.listen( process.env.PORT ||3000)
 
 module.exports = app
